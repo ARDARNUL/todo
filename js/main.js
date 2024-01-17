140fc7d461025ebe85156f0565290e7ffdba7737
ardarnul <obozin1998@mail.ru>
2024-01-17 11:26:22 +0700

    Base

---
+++ js/main.js
@@ -0,0 +1,21 @@
+vue.component('list',{
+    template:`
+    <div class=""main">
+        <div id="cart1 cart">
+            <button v-on:click = " ">+</button>
+        </div>
+        <div id="cart2 cart">
+            <button v-on:click = " ">+</button>
+        </div>
+        <div id="cart3 cart">
+            <button v-on:click = " ">+</button>
+        </div>
+    </div>
+`
+})
+
+let todo = new Vue({
+    el: '#todo'
+
+})
+
